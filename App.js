efe322bfa7be9c77b0be221773400f65fcb18fdd
Ângelo Baracho <92029180+Anbfer@users.noreply.github.com>
2023-11-26 20:06:43 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useFonts } from "expo-font";
+import { useFonts } from 'expo-font';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Dimensions } from 'react-native';
@@ -8,22 +8,22 @@ import Agendamentos from './components/telas/Agendamentos';
 import Agencias from './components/telas/Agencias';
 import CarrosDisponiveis from './components/telas/CarrosDisponiveis';
 import TelaInicial from './components/telas/TelaInicial';
+import Pagamento from './components/telas/Pagamento';
+import ConfirmaPagamento from './components/telas/ConfirmaPagamento';
+import ConfirmarAgendamentos from './components/telas/ConfirmarAgendamento';
 
 const Stack = createNativeStackNavigator();
 
 import Menu from './components/telas/Menu';
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
-
 function App() {
   const [hideSplashScreen, setHideSplashScreen] = React.useState(true);
 
   const [fontsLoaded, error] = useFonts({
-    "Inter-Regular": require("./assets/fonts/Inter-Regular.ttf"),
-    "Baloo2-SemiBold": require("./assets/fonts/Baloo2-SemiBold.ttf"),
-    "Roboto-Light": require("./assets/fonts/Roboto-Light.ttf"),
-    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    'Inter-Regular': require('./assets/fonts/Inter-Regular.ttf'),
+    'Baloo2-SemiBold': require('./assets/fonts/Baloo2-SemiBold.ttf'),
+    'Roboto-Light': require('./assets/fonts/Roboto-Light.ttf'),
+    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
   });
 
   if (!fontsLoaded && !error) {
@@ -32,11 +32,17 @@ function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ header: () => <Menu /> }}>
+      <Stack.Navigator
+        screenOptions={{
+          header: ({ navigation }) => <Menu navigation={navigation} />,
+        }}>
         <Stack.Screen name="Tela Inicial" component={TelaInicial} />
         <Stack.Screen name="Agendamentos" component={Agendamentos} />
         <Stack.Screen name="Agencias" component={Agencias} />
         <Stack.Screen name="Carros Disponiveis" component={CarrosDisponiveis} />
+        <Stack.Screen name="Pagamento" component={Pagamento} />
+        <Stack.Screen name="Confirma Pagamento" component={ConfirmaPagamento} />
+        <Stack.Screen name="Confirma Agendamento" component={ConfirmarAgendamentos} />
       </Stack.Navigator>
     </NavigationContainer>
   );
